b75b59cd68e736a4cb8842479ed87691fd72db53
Mateusz Zalega <mateusz@appliedsourcery.com>
2019-10-19 20:04:54 +0200

    WIP

--- config.hh
+++ config.hh
@@ -11,47 +11,6 @@ public:
 
     double operator[](const std::string &key) const;
 
-    /*
-     *  config items
-     */
-
-    /*
-    // pixels
-    int window_width;
-
-    // pixels 
-    int window_height;
-
-    int fps;
-
-    // shots/second
-    float ship_shooting_freq;
-
-    // meters/second
-    float ship_bullet_velocity;
-
-    // meters/second
-    float ship_forward_velocity;
-
-    // radians/second
-    float asteroid_angular_velocity_range;
-
-    // newtons
-    float ship_thrust;
-
-    // kilograms
-    float ship_mass;
-
-    // number/second
-    float asteroid_appearance_frequency;
-
-    // number/second
-    float asteroid_appearance_frequency_increase;
-
-    int asteroid_hitpoint_range;
-    int projectile_hitpoint_range;
-    */
-
 private:
     void fill_items(std::string config);
     void fill_defaults();
